433149ae74dcd5c8004eede21a817ac92c03b6c5
Aniket Vasane <aniketvasanevijay@gmail.com>
2026-01-23 19:03:41 +0530

    Create CRUD functionality and frontend for listing, detail, and edit views

--- app.js
+++ app.js
@@ -4,9 +4,16 @@ const port = 8080;
 const mongoose = require('mongoose');
 const Listing = require("./models/listing.js");
 const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname,"/public")));
+
+app.engine("ejs",ejsMate);
 
 const Mongo_URL = "mongodb://127.0.0.1:27017/staynest";
 
@@ -28,6 +35,17 @@ app.get("/listing",async(req,res)=>{
    res.render("listings/index",{allListing});
 });
 
+//new route
+app.get("/listing/new",(req,res)=>{
+    res.render("listings/new.ejs");
+})
+
+app.post("/listing",async(req,res)=>{
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    console.log(newListing);
+    res.redirect("/listing");
+})
 // app.get("/testlisting",async(req,res)=>{
 //     let sampleListing = new Listing({
 //         title : "Cozy Beachside Villa",
@@ -41,6 +59,35 @@ app.get("/listing",async(req,res)=>{
 //     console.log("Data saved successfully ");
 //     res.send("DEmo Data save succssfully");
 // })
+
+// edit route
+app.get("/listing/:id/edit",async(req,res)=>{
+    let{id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit",{listing});
+})
+
+
+//show route
+app.get("/listing/:id",async(req,res)=>{
+    const {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs",{listing});
+});
+
+app.put("/listing/:id",async(req,res)=>{
+    const {id} = req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    res.redirect(`/listing/${id}`);
+})
+
+//DELETE ROUTE
+app.delete("/listing/:id",async(req,res)=>{
+    const {id} = req.params;
+    const deletedlisting =await Listing.findByIdAndDelete(id);
+    console.log(deletedlisting);
+    res.redirect("/listing");
+})
 
 app.get("/",(req,res)=>{
     res.send("hello this is demo ");
--- models/listing.js
+++ models/listing.js
@@ -6,16 +6,16 @@ const listingSchema = new Schema({
         required:true
     },
     description : String,
-     image: {
-    filename: {
-      type: String,
-      default: "listingimage"
+    image: {
+        filename: {
+        type: String,
+        default: "listingimage"
+        },
+        url: {
+        type: String,
+        default: "https://images.unsplash.com/photo-1552733407-5d5c46c3bb3b"
+        }
     },
-    url: {
-      type: String,
-      default: "https://images.unsplash.com/photo-1552733407-5d5c46c3bb3b"
-    }
-  },
     price : Number,
     location : String,
     country : String
